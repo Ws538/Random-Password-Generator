3ee05bb75cf0ac5e611607dbc5a9c295f6e224f4
Ws538 <williamsu668@gmail.com>
2022-11-25 15:08:56 +1100

    added characters-function

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// 1. function: created window prompt when button clicked
-// 2. prompt asks how many characters
-// 3. prompt beetween 8 - 128 characters, if not, then loop prompt characters must be inbetween 8-128
-// 4. prompt ask for character types including special characters / numbers / lower/upper case 
-// 5. 
+// 1. function: created window prompt when button clicked, prompt asks how many characters
+// 2. prompt beetween 8 - 128 characters, if not, then loop prompt characters must be inbetween 8-128
+// 3. prompt ask for character types including special characters / numbers / lower/upper case 
+// 4. 
 
 
 
@@ -11,36 +10,29 @@
 
 // Assignment Code
 
-//2.
+//1.
 var generateBtn = document.querySelector("#generate");
 
   function promptMe() {
-    var usergenerate = window.prompt("How long do you want your password")
+    var usergenerate = window.prompt("How long do you want your password? (8-128 characters) ")
     console.log(usergenerate)
     
   } 
-
-//3.   
-
-//4.
+   
+//3.
 function characterType() {
 
-var characters = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmn"
-
-var result = "";
-
-for (var i = 0 ; i < characters.length; i++ ) {
-  result += characters.charAt(Math.floor(Math.random() * characters.length));
-}
-
-return result;
-}
-
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz1234567890?!@#$%^&*"
   
-
-
-
-
+  var result = "";
+  
+  for (var i = 0 ; i < characters.length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  
+  return result;
+  }
+  
 // // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
